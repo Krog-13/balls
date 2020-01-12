bb08ba5a4b5d3a2693661785b54329871d41b82e
krog <virtualspook@gmail.com>
2020-01-12 16:04:29 +0600

    Модификация ООП рефакторинг

--- ball_tack.py
+++ ball_tack.py
@@ -12,14 +12,41 @@ l = tk.Label(root, anchor='sw', bg='black', width=20,fg='white')
 l.pack()
 l['text']='0'
 i=0
-def new_ball():
-    global x,y,r,t
-    canv.delete('all')
-    x = rnd(100,500)
-    y = rnd(100,250)
-    r = rnd(30,50)
-    t=canv.create_oval(x-r,y-r,x+r,y+r, fill=choice(colors), width=0)
-    root.after(2000,new_ball)
+
+
+class one_ball:
+    def __init__(self):
+        self.r = 20
+        self.x = 30
+        self.y = 40
+        self.dx, self.dy = (+1, +1)
+        self.ball_1 = canv.create_oval(10,20,50,60, fill='green', activefill='red',outline='blue')
+
+
+    def move(self):
+        self.x += 2
+        self.y += 2
+    def show(self):
+        canv.move(self.ball_1, self.x, self.y)
+    def bank(self):
+        print('Degrise')
+        
+
+def canv_clik(event):
+    print('I can see that', event.x, event.y)
+
+def tik_tok():
+    for i in range(1):
+        a.move()
+        a.show()
+    root.after(800, tik_tok)
+def main():
+    global balls,a
+    canv.bind('<Button-1>', canv_clik)
+    #balls = [one_ball() for i in range(2)]
+    a = one_ball()
+    tik_tok()
+    tk.mainloop()
 
 def score():
     global i
@@ -28,13 +55,16 @@ def score():
 
 def click(event):
     a = sqrt(((x-event.x)**2) + ((y-event.y)**2))
+    canv.itemconfig(t, fill='green',outline='red',width=5)
     if a <= r:
-        canv.itemconfig(t, fill='green',outline='red',width=5)
+        time.sleep(1)
         canv.delete(t)
         score()
     else:
         print('faill')
 
-new_ball()
-canv.bind('<Button-3>', click)
-tk.mainloop()
+if __name__ == '__main__':
+    main()
+
+#canv.bind('<Button-3>', click)
+#tk.mainloop()
